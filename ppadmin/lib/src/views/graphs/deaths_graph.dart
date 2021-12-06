92d60392f09bffc710ba2e485066b2d0b49c44c6
Pratik <pratik@farmsetu.co>
2021-12-07 01:52:51 +0530

    icon changed and filtering button for home screens maps

--- ppadmin/lib/src/views/graphs/deaths_graph.dart
+++ ppadmin/lib/src/views/graphs/deaths_graph.dart
@@ -26,37 +26,56 @@ class _DeathsGraphState extends State<DeathsGraph> {
         padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
             color: Colors.grey[200], borderRadius: BorderRadius.circular(15)),
-        height: MediaQuery.of(context).size.height * 0.7,
         width: double.infinity,
-        child: SfCartesianChart(
-            primaryXAxis: CategoryAxis(),
-            // Chart title
-            title: ChartTitle(text: ILLEGAL_WORKS),
-            // Enable legend
-            // legend: Legend(isVisible: true),
-            // Enable tooltip
-            tooltipBehavior: _tooltipBehavior,
-            series: <ColumnSeries<DeathGraphData, String>>[
-              ColumnSeries<DeathGraphData, String>(
-                  name: ILLEGAL_WORKS,
-                  color: Colors.deepOrange,
-                  dataSource: <DeathGraphData>[
-                    DeathGraphData('Jan', 23),
-                    DeathGraphData('Feb', 12),
-                    DeathGraphData('Mar', 21),
-                    DeathGraphData('Apr', 32),
-                    DeathGraphData('May', 18),
-                    DeathGraphData('Jun', 29),
-                    DeathGraphData('Jul', 12),
-                    DeathGraphData('Aug', 27),
-                    DeathGraphData('Sep', 22),
-                    DeathGraphData('Oct', 9)
-                  ],
-                  xValueMapper: (DeathGraphData crimes, _) => crimes.month,
-                  yValueMapper: (DeathGraphData crimes, _) => crimes.count,
-                  // Enable data label
-                  dataLabelSettings: const DataLabelSettings(isVisible: true))
-            ]));
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerRight,
+              child: FilterButton(onTap: () async {
+                await showDialog(
+                    context: context,
+                    builder: (context) {
+                      return const FilterDataWidget();
+                    });
+              }),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: SfCartesianChart(
+                  primaryXAxis: CategoryAxis(),
+                  // Chart title
+                  title: ChartTitle(text: ILLEGAL_WORKS),
+                  // Enable legend
+                  // legend: Legend(isVisible: true),
+                  // Enable tooltip
+                  tooltipBehavior: _tooltipBehavior,
+                  series: <ColumnSeries<DeathGraphData, String>>[
+                    ColumnSeries<DeathGraphData, String>(
+                        name: ILLEGAL_WORKS,
+                        color: Colors.deepOrange,
+                        dataSource: <DeathGraphData>[
+                          DeathGraphData('Jan', 23),
+                          DeathGraphData('Feb', 12),
+                          DeathGraphData('Mar', 21),
+                          DeathGraphData('Apr', 32),
+                          DeathGraphData('May', 18),
+                          DeathGraphData('Jun', 29),
+                          DeathGraphData('Jul', 12),
+                          DeathGraphData('Aug', 27),
+                          DeathGraphData('Sep', 22),
+                          DeathGraphData('Oct', 9)
+                        ],
+                        xValueMapper: (DeathGraphData crimes, _) =>
+                            crimes.month,
+                        yValueMapper: (DeathGraphData crimes, _) =>
+                            crimes.count,
+                        // Enable data label
+                        dataLabelSettings:
+                            const DataLabelSettings(isVisible: true))
+                  ]),
+            ),
+          ],
+        ));
   }
 }
 
